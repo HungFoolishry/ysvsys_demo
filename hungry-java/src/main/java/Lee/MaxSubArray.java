ce8f97c4876409903b7db802e8602d34d7b3e8f1
姚俊超 <junchaoyao@clubfactory.com>
2020-08-17 10:11:34 +0800

    add max sub array

---
+++ hungry-java/src/main/java/Lee/MaxSubArray.java
@@ -0,0 +1,32 @@
+package Lee;
+
+import java.util.Arrays;
+
+/**
+ * description: 最大子序和
+ *
+ * @author JunchaoYao
+ * @date 2020-08-14 15:58
+ **/
+public class MaxSubArray {
+    public static int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int max = 0;
+        for(int num: nums){
+            if (max > 0) {
+                max = max + num;
+            } else {
+                max = num;
+            }
+            ans = Math.max(max, ans);
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(input));
+    }
+
+}
