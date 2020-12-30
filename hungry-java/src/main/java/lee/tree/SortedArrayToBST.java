85052980b8fe1284eba81911a543350a599ecb97
姚俊超 <junchaoyao@clubfactory.com>
2020-12-30 10:28:38 +0800

    fix list to BST

--- hungry-java/src/main/java/lee/tree/SortedArrayToBST.java
+++ hungry-java/src/main/java/lee/tree/SortedArrayToBST.java
@@ -17,18 +17,18 @@ public class SortedArrayToBST {
 
     class Solution {
         public TreeNode sortedArrayToBST(int[] nums) {
-            return buildNode(nums, 0, nums.length - 1);
+            return helper(nums, 0, nums.length-1);
         }
 
-        private TreeNode buildNode(int[] nums, int l, int r) {
+        private TreeNode helper(int[] nums, int l, int r) {
             if (l > r) {
                 return null;
             }
-            int mid = (l + r) / 2;
-            TreeNode root = new TreeNode(nums[mid]);
-            root.left = buildNode(nums, 0, mid-1);
-            root.right = buildNode(nums, mid + 1, r);
-            return root;
+            int mid = (l + r)/2;
+            TreeNode node = new TreeNode(nums[mid]);
+            node.left = helper(nums, l, mid - 1);
+            node.right = helper(nums, mid + 1, r);
+            return node;
         }
     }
 }
