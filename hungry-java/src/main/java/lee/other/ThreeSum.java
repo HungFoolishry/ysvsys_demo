7fbd05be66f7abeb49b26b838fbb27441ecdc9f2
姚俊超 <junchaoyao@clubfactory.com>
2021-01-18 10:21:03 +0800

    三数之和

---
+++ hungry-java/src/main/java/lee/other/ThreeSum.java
@@ -0,0 +1,66 @@
+package lee.other;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * description: 三数之和
+ *
+ * @author JunchaoYao
+ * @date 2021-01-15 22:02
+ **/
+public class ThreeSum{
+    static class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            int len = nums.length;
+            if (len < 3) {
+                return ans;
+            }
+            Arrays.sort(nums);
+            if (nums[0] > 0) {
+                return ans;
+            }
+            for (int i = 0; i < len; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] > 0) {
+                    break;
+                }
+                int l = i + 1;
+                int r = len - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[l] + nums[r];
+                    if (sum == 0) {
+                        ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (sum > 0) {
+                        r--;
+                    } else {
+                        l++;
+                    }
+                    if (nums[l] > -nums[i]) {
+                        break;
+                    }
+
+                }
+            }
+            return ans;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
